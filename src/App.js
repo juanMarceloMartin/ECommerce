52f7c60b7a6ccef076f64415eac78e06dd28cf16
Marce <jmarce_83@hotmail.com>
2021-06-01 21:02:45 -0300

    ant design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Layout } from 'antd';
 
 function App() {
+  
+  const { Header, Footer, Sider, Content } = Layout;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+    <Layout>
+      <Header>Header</Header>
+      <Layout className="layout-body">
+        <Sider>Sider</Sider>
+        <Content>Content</Content>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
+  </>
   );
 }
 
