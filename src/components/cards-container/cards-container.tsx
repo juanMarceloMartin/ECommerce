05b42f5a0c1b8a6cb23367cd5f1ab31f7287439e
Marcelo <marcelo.martin@softvision.com>
2021-06-26 11:42:52 -0300

    removed antd elements

--- src/components/cards-container/cards-container.tsx
+++ src/components/cards-container/cards-container.tsx
@@ -3,13 +3,11 @@ import { ProductsReducerActions } from '../../reducers/products-reducer';
 import { useSelector, useDispatch } from 'react-redux';
 import IProduct from '../../commons/interfaces/IProduct';
 import IProductState from '../../commons/interfaces/IProductState';
-import { Layout } from 'antd';
 import ProductCard from '../product-card/product-card';
 import Sidebar from '../sidebar/sidebar';
 import { Grid } from '@material-ui/core';
 
 const CardsContainer: FC = () => {
-    const { Content } = Layout;
     const dispatch = useDispatch();
     const productsList = useSelector((state: IProductState) => state.products.list);
     const categories = useSelector((state: IProductState) => state.products.categories);
@@ -26,16 +24,18 @@ const CardsContainer: FC = () => {
     }, [])
 
     return (
-        <>
-            <Sidebar title="Categories" items={categories} />
-            <Content>
+        <Grid container>
+            <Grid item md={2}>
+                <Sidebar title="Categories" items={categories} />
+            </Grid>
+            <Grid item md={10}>
                 <Grid container>
                     {
                         productsList?.map((product: IProduct) => <ProductCard key={product.id} product={product} />)
                     }
                 </Grid>
-            </Content>
-        </>
+            </Grid>
+        </Grid>
     )
 }
 
